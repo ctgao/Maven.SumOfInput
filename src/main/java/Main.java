9ea0777c0943c3d3b0a0fa6b74a71f83561256ee
Christine <christine@Zipcoders-MacBook-Pro-51.local>
2023-06-28 18:53:39 -0400

    i think i did the extra credit idk tho :)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,26 +1,36 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args){
         
         int number = getInputNumber();
-        
         int sum = sumOfNumbers(number);
-        
-        System.out.println(sum);
+        int gausSum = gaussianSumOfNumbers(number);
 
+        System.out.println(sum);
+        System.out.println(gausSum);
+
+        //1 million times and cases
+        compareSpeed(100000);
     }
     
     // do this one and the lab is complete. really.
     // return the sum of 0 to n... if n == 3, the result should be 6
-    int sumOfNumbers(int n) {
-        return 0;
+    static int sumOfNumbers(int n) {
+        int sum = 0;
+        for(int i = 1; i <= n; i++){
+            sum += i;
+        }
+        return sum;
     }
     
     
-    int getInputNumber() {
+    static int getInputNumber() {
         Scanner scan = new Scanner(System.in);
         System.out.print("Enter any number: ");
 
@@ -35,11 +45,32 @@ public class Main {
     
     
     // for Extra Credit
-    int gaussianSumOfNumbers(int n) {
-        return 0;
+    static int gaussianSumOfNumbers(int n) {
+        return (n * (n + 1) / 2);
     }
    
     // for Extra extra credit - compare the methods and show which one is faster
     // google "how to time some java code"
     // use "System.currentTimeMillis()" to ask the system what time it is.
+    static void compareSpeed(int numTimes){
+        // do the sum of numbers function and time it
+        // should be O(n)
+        final long startTime = System.currentTimeMillis();
+        for(int i = 0; i < numTimes; i++){
+            sumOfNumbers(i);
+        }
+        final long endTime = System.currentTimeMillis();
+
+        System.out.println("Total execution time for " + numTimes + " number of times: " + (endTime - startTime));
+
+        // do the gaussian sum of numbers function and time it
+        // should be O(1)
+        final long startGaus = System.currentTimeMillis();
+        for(int i = 0; i < numTimes; i++){
+            gaussianSumOfNumbers(i);
+        }
+        final long endGaus = System.currentTimeMillis();
+
+        System.out.println("Total execution time for " + numTimes + " number of times: " + (endGaus - startGaus));
+    }
 }
